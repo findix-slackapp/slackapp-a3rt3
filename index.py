2471d16e3f4583d76f0a0f2ba2fc1caee5e4befe
吉川　貴子 <uu036289@CA0933.local>
2019-04-03 16:14:44 +0900

    SlackのEvent APIの認証

--- index.py
+++ index.py
@@ -3,10 +3,9 @@ import datetime
 
 
 def handler(event, context):
-    data = {
-        'output': 'Hello World',
-        'timestamp': datetime.datetime.utcnow().isoformat()
-    }
-    return {'statusCode': 200,
-            'body': json.dumps(data),
-            'headers': {'Content-Type': 'application/json'}}
+
+    # SlackのEvent APIの認証
+    if "challenge" in event:
+        return event["challenge"]
+    
+    return "OK"
