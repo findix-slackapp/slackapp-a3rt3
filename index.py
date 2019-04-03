133596592c9cd574ed7622c28d2775b987e2760a
吉川　貴子 <uu036289@CA0933.local>
2019-04-03 18:36:15 +0900

    slackに返答する処理を追加

--- index.py
+++ index.py
@@ -1,9 +1,24 @@
 import json
-import datetime
-from urllib.parse import parse_qs
+import logging
+import urllib.request
+import os
+
+print('Loading function... ')
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 def handler(event, context):
+    #getenv
+    OAUTH_TOKEN = os.environ['OAUTH_TOKEN']
+    BOT_TOKEN = os.environ['BOT_TOKEN']
+    HOOK_KEYWORD = os.environ['HOOK_KEYWORD']
+    REPLY_WORD = os.environ['REPLY_WORD']
+    BOT_NAME = os.environ['BOT_NAME']
 
+    #受信したjsonをLogsに出力
+    logging.info(json.dumps(event))
+
+    print (type(event))
     # json処理
     if 'body' in event:
         body = json.loads(event.get('body'))
@@ -13,20 +28,33 @@ def handler(event, context):
         logger.error('unexpected event format')
         return {'statusCode': 500, 'body': 'error:unexpected event format'}
 
-    # SlackのEvent APIの認証
+    #url_verificationイベントに返す
     if 'challenge' in body:
         challenge = body.get('challenge')
+        logging.info('return challenge key %s:', challenge)
         return {
             'isBase64Encoded': 'true',
             'statusCode': 200,
             'headers': {},
             'body': challenge
         }
-
-    data = {
-        'output': 'hellow',
-        'timestamp': datetime.datetime.utcnow().isoformat()
-    }
-    return {'statusCode': 200,
-            'body': json.dumps(data),
-            'headers': {'Content-Type': 'application/json'}}
+    #SlackMessageに特定のキーワードが入っていたときの処理（poopに反応して処理します）
+    if body.get('event').get('text') == HOOK_KEYWORD:
+        logger.info('hit!: %s', HOOK_KEYWORD)
+        url = 'https://slack.com/api/chat.postMessage'
+        headers = {
+            'Content-Type': 'application/json; charset=UTF-8',
+            'Authorization': 'Bearer {0}'.format(BOT_TOKEN)
+        }
+        data = {
+            'token': OAUTH_TOKEN,
+            'channel': body.get('event').get('channel'),
+            'text': REPLY_WORD,
+            'username': BOT_NAME
+        }
+        # POST処理　
+        req = urllib.request.Request(url, data=json.dumps(data).encode('utf-8'), method='POST', headers=headers)
+        res = urllib.request.urlopen(req)
+        logger.info('post result: %s', res.msg)
+        return {'statusCode': 200, 'body': 'ok'}
+    return {'statusCode': 200, 'body': 'quit'}
